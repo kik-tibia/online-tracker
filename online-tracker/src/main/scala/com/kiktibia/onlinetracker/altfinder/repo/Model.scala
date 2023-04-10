0cd559a4fa1aae6c1d7a78feb356638e25c5dced
Kikaro <kik-tibia@users.noreply.github.com>
2023-04-10 12:38:09 +0100

    Better formatting for history printer

--- online-tracker/src/main/scala/com/kiktibia/onlinetracker/altfinder/repo/Model.scala
+++ online-tracker/src/main/scala/com/kiktibia/onlinetracker/altfinder/repo/Model.scala
@@ -1,9 +1,17 @@
 package com.kiktibia.onlinetracker.altfinder.repo
 
-import java.time.OffsetDateTime
+import java.time.format.DateTimeFormatter
+import java.time.{LocalDateTime, OffsetDateTime, ZoneId}
 
 object Model {
   case class OnlineSegment(characterId: Long, start: Long, end: Long)
 
-  case class OnlineDateSegment(characterName: String, start: OffsetDateTime, end: OffsetDateTime)
+  case class OnlineDateSegment(characterName: String, start: OffsetDateTime, end: OffsetDateTime) {
+    override def toString: String =
+      s"$characterName: ${start.atLocal} - ${end.atLocal}"
+
+    extension (d: OffsetDateTime)
+      private def atLocal: String =
+        d.atZoneSameInstant(ZoneId.systemDefault()).toLocalDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
+  }
 }
