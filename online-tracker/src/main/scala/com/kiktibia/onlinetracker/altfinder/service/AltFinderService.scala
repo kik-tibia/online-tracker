25c331ac9b2774a132f84b9861c680dc5371a51c
Kikaro <kik-tibia@users.noreply.github.com>
2023-04-11 10:29:28 +0100

    Add method to manually compare two sets of characters

--- online-tracker/src/main/scala/com/kiktibia/onlinetracker/altfinder/service/AltFinderService.scala
+++ online-tracker/src/main/scala/com/kiktibia/onlinetracker/altfinder/service/AltFinderService.scala
@@ -33,8 +33,8 @@ class AltFinderService[F[_]: Sync](repo: AltFinderRepoAlg[F]) {
     for
       mainSegments <- repo.getOnlineTimes(characterNames)
       matchesToCheck <- repo.getPossibleMatches(characterNames)
-      _ <- Logger[F].info(s"${matchesToCheck.length} rows to analyse")
-      adj = getAdjacencies(mainSegments, matchesToCheck).take(20)
+      _ <- Logger[F].info(s"${matchesToCheck.length} rows to analyse from ${mainSegments.length} segments")
+      adj = getAdjacencies(mainSegments, matchesToCheck, includeClashes = false).take(20)
       results <- adj.map(a => repo.getCharacterName(a.characterId).map { i =>
         a.copy(characterName = Some(i))
       }).sequence
@@ -42,11 +42,28 @@ class AltFinderService[F[_]: Sync](repo: AltFinderRepoAlg[F]) {
     yield ()
   }
 
-  private def getAdjacencies(mainHistory: List[OnlineSegment], others: List[OnlineSegment]): List[CharacterAdjacencies] = {
+  def checkForClashes(characterNames: List[String], toCheck: List[String]): F[Unit] = {
+    for
+      mainSegments <- repo.getOnlineTimes(characterNames)
+      toCheckSegments <- repo.getOnlineTimes(toCheck)
+      _ <- Logger[F].info(s"${toCheckSegments.length} rows to analyse from ${mainSegments.length} segments")
+      adj = getAdjacencies(mainSegments, toCheckSegments, includeClashes = true)
+      results <- adj.map(a => repo.getCharacterName(a.characterId).map { i =>
+        a.copy(characterName = Some(i))
+      }).sequence
+      _ <- results.map(i => Logger[F].info(i.toString)).sequence
+    yield ()
+  }
+
+  private def getAdjacencies(mainHistory: List[OnlineSegment], others: List[OnlineSegment], includeClashes: Boolean): List[CharacterAdjacencies] = {
     val characterHistories = others.groupBy(_.characterId).toList.map(i => CharacterLoginHistory(i._1, i._2))
-    characterHistories.filter(h => countClashes(mainHistory, h.segments) == 0)
-      .map(h => CharacterAdjacencies(h.characterId, None, countAdjacencies(mainHistory, h.segments), 0, h.segments.length))
-      .sortBy(-_.adjacencies)
+
+    characterHistories.flatMap { h =>
+      val clashes = countClashes(mainHistory, h.segments)
+      if clashes == 0 || includeClashes then
+        Some(CharacterAdjacencies(h.characterId, None, countAdjacencies(mainHistory, h.segments), clashes, h.segments.length))
+      else None
+    }.sortBy(-_.adjacencies)
   }
 
   private def countClashes(mainHistory: List[OnlineSegment], other: List[OnlineSegment]): Int = {
