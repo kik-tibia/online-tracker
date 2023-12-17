f389eaead1512f711a2c3b6ec8d71e448f855939
Kikaro <kik-tibia@users.noreply.github.com>
2023-12-17 19:45:54 +0000

    Update TibiaData to v4

--- online-tracker/tracker/src/main/scala/com/kiktibia/onlinetracker/tracker/tibiadata/response/Information.scala
+++ online-tracker/tracker/src/main/scala/com/kiktibia/onlinetracker/tracker/tibiadata/response/Information.scala
@@ -1,6 +1,7 @@
 package com.kiktibia.onlinetracker.tracker.tibiadata.response
 
-case class Information(
-  api_version: Double,
-  timestamp: String
-)
+case class Api(version: Int, release: String, commit: String)
+
+case class Status(http_code: Int)
+
+case class Information(api: Api, timestamp: String, status: Status)
